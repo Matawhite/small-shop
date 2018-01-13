68dd87c0a8e64c2be6357eff7ab95557a0227a26
Matt White <mattwhite614@gmail.com>
2018-01-13 11:01:16 -0600

    http service done

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HttpService from '../services/http-service'
+
+const http = new HttpService()
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.loadData = this.loadData.bind(this)
+  }
+
+  loadData = () => {
+    http.getProducts().then( products => {
+      console.log(products)
+    }, error => {
+      console.log(error)
+    })
+  }
+
+  componentWillMount(){
+    this.loadData()
+  }
+
   render() {
     return (
       <div className="App">
